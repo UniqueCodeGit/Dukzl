31791aef5207784d544d04b229f5446f32f0fe7e
zero734kr <zero734kr@gmail.com>
2021-01-29 15:11:37 -0300

    Update run.py

--- run.py
+++ run.py
@@ -21,21 +21,25 @@ class Dukzl(commands.Bot):
         self.Wonstein = discodo.DPYClient(self)
 
     async def on_message(self, message):
-        if not message.author.bot:
-            return await self.process_commands(message)
+        if message.author.bot:
+            return 
+        
+        return await self.process_commands(message)
 
     async def on_ready(self):
         self.logger.info(f"Dukzl loaded as {self.user.name}")
 
     async def on_command_error(self, context, exception):
-        if isinstance(exception, commands.CommandInvokeError):
-            embed = discord.Embed(
-                title=f"{context.command.name} 수행 중 에러 발생",
-                description="Dukzl 서버에서 버그 제보를 해주세요!",
-                color=config.COLOR,
-            )
-            embed.add_field(name="에러 내용", value=f"```{exception}```")
-            await context.send(embed=embed)
+        if not isinstance(exception, commands.CommandInvokeError):
+            return
+        
+        embed = discord.Embed(
+            title=f"{context.command.name} 수행 중 에러 발생",
+            description="Dukzl 서버에서 버그 제보를 해주세요!",
+            color=config.COLOR,
+        )
+        embed.add_field(name="에러 내용", value=f"```{exception}```")
+        await context.send(embed=embed)
 
 
 bot = Dukzl()
